8f2bcfe5262db461b4e34e36dcec0716ec30d4ec
Destira Lestari <100773981+desttari@users.noreply.github.com>
2023-02-15 23:06:41 +0700

    Delete Mahasiswa.py

--- LATIHAN1/PYTHON/FIX/Mahasiswa.py
+++
@@ -1,8 +0,0 @@
-class Mahasiswa:
-    def __init__(self, nama, nim, prog_studi, fakultas, umur, email):
-        self.nama = nama
-        self.nim = nim
-        self.prog_studi = prog_studi
-        self.fakultas = fakultas
-        self.umur = umur
-        self.email = email
