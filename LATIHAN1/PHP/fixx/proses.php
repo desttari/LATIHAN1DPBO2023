8cf033d3416719640c10488dbca69e480defbc08
Destira Lestari <100773981+desttari@users.noreply.github.com>
2023-02-15 22:22:30 +0700

    Update proses.php

--- LATIHAN1/PHP/fixx/proses.php
+++ LATIHAN1/PHP/fixx/proses.php
@@ -1,27 +1,82 @@
-<?php
-//proses ambil data yg dinput kemudian disimpan ke dalam file.txt
-if (isset($_POST['submit'])) {
-	$nim = $_POST['nim'];
-	$nama = $_POST['nama'];
-	$prodi = $_POST['prodi'];
-	$fakultas = $_POST['fakultas'];
-	$angkatan = $_POST['angkatan'];
-	$email = $_POST['email'];
-    //semua data yg diinput dimasukan ke variabel data
-	$data = [
-		'nim' => $nim,
-		'nama' => $nama,
-		'prodi' => $prodi,
-		'fakultas' => $fakultas,
-		'angkatan' => $angkatan,
-		'email' => $email
-	];
-    //memasukan data ke file txt
-	$file = fopen("data.txt", "a+");
-	fputcsv($file, $data);
-	fclose($file);
 
-	echo "Data berhasil disimpan!";
+
+<?php
+//kelas mahasiswa
+class Mahasiswa {
+	private $nim;
+	private $nama;
+	private $prodi;
+	private $fakultas;
+	private $angkatan;
+	private $email;
+
+    //construct
+	public function __construct($nim, $nama, $prodi, $fakultas, $angkatan, $email) {
+		$this->nim = $nim;
+		$this->nama = $nama;
+		$this->prodi = $prodi;
+		$this->fakultas = $fakultas;
+		$this->angkatan = $angkatan;
+		$this->email = $email;
+	}
+
+    //getter setter
+	public function setNim($nim) {
+		$this->nim = $nim;
+	}
+
+	public function getNim() {
+		return $this->nim;
+	}
+
+	public function setNama($nama) {
+		$this->nama = $nama;
+	}
+
+	public function getNama() {
+		return $this->nama;
+	}
+
+	public function setProdi($prodi) {
+		$this->prodi = $prodi;
+	}
+
+	public function getProdi() {
+		return $this->prodi;
+	}
+
+	public function setFakultas($fakultas) {
+		$this->fakultas = $fakultas;
+	}
+
+	public function getFakultas() {
+		return $this->fakultas;
+	}
+
+	public function setAngkatan($angkatan) {
+		$this->angkatan = $angkatan;
+	}
+
+	public function getAngkatan() {
+		return $this->angkatan;
+	}
+
+	public function setEmail($email) {
+		$this->email = $email;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
 }
+//semua data yg diinput dimasukan ke variabel data
+$mahasiswa = new Mahasiswa($_POST['nim'], $_POST['nama'], $_POST['prodi'], $_POST['fakultas'], $_POST['angkatan'], $_POST['email']);
+//memasukan data ke file txt, dengan append baca tulis file
+$file = fopen("data.txt", "a+");
+fputcsv($file, [$mahasiswa->getNim(), $mahasiswa->getNama(), $mahasiswa->getProdi(), $mahasiswa->getFakultas(), $mahasiswa->getAngkatan(), $mahasiswa->getEmail()]);
+fclose($file);
+
+echo "Data berhasil disimpan!";
 ?>
+
 
